13575f2231a8dd39d0e7f9ff0413c878e2b051d6
selfrideofficial-wq <selfrideofficial@gmail.com>
2025-12-14 13:16:56 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showTab(tab) {
-  document.querySelectorAll('.tab').forEach(t => t.classList.add('hidden'));
-  document.getElementById(tab).classList.remove('hidden');
-}
-
-function generate() {
-  document.getElementById('result').classList.remove('hidden');
-}
